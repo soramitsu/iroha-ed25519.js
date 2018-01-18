5d3c99d0c83be44cab93f65cda0fcf997e39673d
Vyacheslav Bikbaev <la.sintez@gmail.com>
2018-01-18 15:24:32 +0300

    chore: standard linting & styleguide

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var Module = require('./lib/ed25519.min.js')
 
-exports.createKeyPair = function(){
+exports.createKeyPair = function () {
   var pubKeyPtr = Module._malloc(32)
   var pubKey = new Uint8Array(Module.HEAPU8.buffer, pubKeyPtr, 32)
 
   var privKeyPtr = Module._malloc(32)
   var privKey = new Uint8Array(Module.HEAPU8.buffer, privKeyPtr, 32)
-  
+
   Module._ed25519_create_keypair(privKeyPtr, pubKeyPtr)
 
-  result = {
+  var result = {
     publicKey: Buffer.from(pubKey),
     privateKey: Buffer.from(privKey)
   }
@@ -17,11 +17,11 @@ exports.createKeyPair = function(){
   Module._free(pubKeyPtr)
   Module._free(privKeyPtr)
 
-  return result;
+  return result
 }
 
-exports.derivePublicKey = function(privateKey){
-  if(!Buffer.isBuffer(privateKey)){
+exports.derivePublicKey = function (privateKey) {
+  if (!Buffer.isBuffer(privateKey)) {
     throw new Error('Input arguments are not buffers!')
   }
 
@@ -34,7 +34,7 @@ exports.derivePublicKey = function(privateKey){
 
   Module._ed25519_derive_public_key(privKeyPtr, pubKeyPtr)
 
-  result = Buffer.from(pubKey)
+  var result = Buffer.from(pubKey)
 
   Module._free(pubKeyPtr)
   Module._free(privKeyPtr)
@@ -42,8 +42,8 @@ exports.derivePublicKey = function(privateKey){
   return result
 }
 
-exports.sign = function(message, publicKey, privateKey){
-  if(!Buffer.isBuffer(message) || !Buffer.isBuffer(publicKey) || !Buffer.isBuffer(privateKey)){
+exports.sign = function (message, publicKey, privateKey) {
+  if (!Buffer.isBuffer(message) || !Buffer.isBuffer(publicKey) || !Buffer.isBuffer(privateKey)) {
     throw new Error('Input arguments are not buffers!')
   }
   var msgLen = message.length
@@ -62,13 +62,13 @@ exports.sign = function(message, publicKey, privateKey){
   var sigPtr = Module._malloc(64)
   var sig = new Uint8Array(Module.HEAPU8.buffer, sigPtr, 64)
 
-  /* 
+  /*
     WARNING: 0 is passed to the _ed25519_sign as 4th argument due to an error in the EMSCRIPTEN.
     In case of EMSCRIPTEN fix and correct build, testcase will fail and this 0 will be removed from here.
   */
   Module._ed25519_sign(sigPtr, msgPtr, msgLen, 0, pubKeyPtr, privKeyPtr)
-  
-  result = Buffer.from(sig)
+
+  var result = Buffer.from(sig)
 
   Module._free(msgPtr)
   Module._free(pubKeyPtr)
@@ -78,8 +78,8 @@ exports.sign = function(message, publicKey, privateKey){
   return result
 }
 
-exports.verify = function(signature, message, publicKey){
-  if(!Buffer.isBuffer(signature) || !Buffer.isBuffer(message) || !Buffer.isBuffer(publicKey)){
+exports.verify = function (signature, message, publicKey) {
+  if (!Buffer.isBuffer(signature) || !Buffer.isBuffer(message) || !Buffer.isBuffer(publicKey)) {
     throw new Error('Input arguments are not buffers!')
   }
 
@@ -96,11 +96,11 @@ exports.verify = function(signature, message, publicKey){
   var sig = new Uint8Array(Module.HEAPU8.buffer, sigPtr, 64)
   sig.set(signature)
 
-  /* 
+  /*
     WARNING: 0 is passed to the _ed25519_verify as 4th argument due to an error in the EMSCRIPTEN.
     In case of EMSCRIPTEN fix and correct build, testcase will fail and this 0 will be removed from here.
   */
-  result = Module._ed25519_verify(sigPtr, msgPtr, msgLen, 0, pubKeyPtr) === 1
+  var result = Module._ed25519_verify(sigPtr, msgPtr, msgLen, 0, pubKeyPtr) === 1
 
   Module._free(msgPtr)
   Module._free(pubKeyPtr)
